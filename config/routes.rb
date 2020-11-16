b67a0812bd949770d08b176e3a0250cfe90d7434
Daniel Wiehoff <daniel.wiehoff+github@gmail.com>
2020-11-16 16:56:37 +0100

    Added controller for spaces

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'spaces/index'
+  get 'spaces/show'
+  get 'spaces/new'
+  get 'spaces/create'
+  get 'spaces/destroy'
+  get 'spaces/edit'
+  get 'spaces/update'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
