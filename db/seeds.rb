2d6f5700a80462b87b6682c050c97da4d165d8ce
Daniel Wiehoff <daniel.wiehoff+github@gmail.com>
2020-11-17 11:37:53 +0100

    Adjusted puts statement on seed file according to review comment

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,4 +9,4 @@ demo_user = User.last
 Space.create!(location: "Erich-Kästner-Str. 11, München", volume: 4, price_per_month: 25, user: demo_user)
 Space.create!(location: "Helene-Mayer-Ring 14, 80809 München",volume: 3, price_per_month: 15, user: demo_user)
 Space.create!(location: "Helene-Mayer-Ring 14, 80809 München", volume: 3, price_per_month: 15, user: demo_user, condition: 'Cannot be used for non-water-proof items', access: 'Accessible by car', description: 'Test description')
-puts "Created three spaces"
+puts "#{Space.count} spaces in the DB. Last one located at #{Space.last.location}"
