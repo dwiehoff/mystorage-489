469052791b73c922d071a593954f9b3351a427b7
charline noga <charlinenoga@gmail.com>
2020-11-17 16:39:44 +0100

    registration/new view/create but not tested

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -4,4 +4,17 @@ class ReservationsController < ApplicationController
     @space = Space.find(params[:space_id])
     @reservation = Reservation.new
   end
+
+  def create
+    @reservation = Reservation.new(reservation_params)
+    @reservation = Reservation.find(params[:space_id])
+    @reservation.save
+    redirect_to space_path(@space)
+  end
+
+  private
+
+  def reservation_params
+    params.require(:reservation).permit(:start_date, :end_date)
+  end
 end
