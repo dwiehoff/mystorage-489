303f00bb8070500fd67931d89824866b349a6517
Daniel Wiehoff <daniel.wiehoff+github@gmail.com>
2020-11-17 12:34:48 +0100

    Added link between space and its reservations

--- app/models/space.rb
+++ app/models/space.rb
@@ -1,5 +1,6 @@
 class Space < ApplicationRecord
   belongs_to :user
+  has_many :reservations, dependent: :destroy
 
   validates :location, presence: true
   validates :volume, numericality: { greater_than: 0 }
