1a36049c3d193242ad4774cc48dc3fbd262d79e2
Daniel Wiehoff <daniel.wiehoff+github@gmail.com>
2020-11-21 19:47:47 +0100

    Styled show view

--- app/helpers/space_helper.rb
+++ app/helpers/space_helper.rb
@@ -1,5 +1,5 @@
 module SpaceHelper
   def display_li_if_exists(attr, label, after = '')
-    "<li><strong>#{label}:</strong> #{@space[attr]} #{after.html_safe}</li>".html_safe if @space[attr].present?
+    "<li class='list-group-item'><strong>#{label}:</strong> #{@space[attr]} #{after.html_safe}</li>".html_safe if @space[attr].present?
   end
 end
